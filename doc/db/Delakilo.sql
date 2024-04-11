84e237aa181acf04a4400162e70b4bf47d3d3ac7
Enrico Marchionni <enrico.marchionni@studio.unibo.it>
2024-04-11 18:04:13 +0200

    Update MySQL database creation file

--- doc/db/Delakilo.sql
+++ doc/db/Delakilo.sql
@@ -22,65 +22,65 @@ USE `Delakilo`;
 -- ______________
 
 CREATE TABLE `USER` (
-     `username` VARCHAR(50) NOT NULL,
+     `Username` VARCHAR(50) NOT NULL,
      -- `email` VARCHAR(50) UNIQUE DEFAULT NULL,
      -- TODO: random string of maximum length 1004 considering that a password has to be of 20 characters at least
-     `salt` VARCHAR(1004) NOT NULL,
-     `password_hash` VARCHAR(1024) NOT NULL,
+     `passwordSalt` VARCHAR(1004) NOT NULL,
+     `passwordHash` VARCHAR(1024) NOT NULL,
      `name` VARCHAR(50) NOT NULL DEFAULT '',
      `surname` VARCHAR(50) NOT NULL DEFAULT '',
      -- `birthday` DATE DEFAULT NULL,
      `bio` VARCHAR(500) NOT NULL DEFAULT '',
-     -- TODO: come default va messa l'URL all'immagine profilo standard
-     `image_URL` VARCHAR(400) NOT NULL,
-     `registration_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-     `n_followers` INT UNSIGNED NOT NULL DEFAULT 0,
-     `n_followed` INT UNSIGNED NOT NULL DEFAULT 0,
-     `n_posts` INT UNSIGNED NOT NULL DEFAULT 0,
-     PRIMARY KEY (`username`)
-) ENGINE = InnoDB;
-
-CREATE TABLE `POST` (
-     `ID_post` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-     `image_URL` VARCHAR(400) NOT NULL,
-     `caption` VARCHAR(1000) NOT NULL DEFAULT '',
+     `imageURL` VARCHAR(400) NOT NULL, -- DEFAULT 'profile.svg', TODO: decide how to do this
+     `nFollowers` INT UNSIGNED NOT NULL DEFAULT 0,
+     `nFollowed` INT UNSIGNED NOT NULL DEFAULT 0,
+     `nPosts` INT UNSIGNED NOT NULL DEFAULT 0,
      `timestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-     `PK_User` VARCHAR(50) NOT NULL,
-     PRIMARY KEY (`ID_post`)
-) ENGINE = InnoDB;
-
-CREATE TABLE `COMMENT` (
-     `ID_comment` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-     `PK_User` VARCHAR(50) NOT NULL,
-     `PK_Post` INT UNSIGNED NOT NULL,
-     `content` VARCHAR(1000) NOT NULL,
-     `PK_Comment_parent` INT UNSIGNED DEFAULT NULL,
-     `timestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-     PRIMARY KEY (`ID_comment`),
-     CONSTRAINT `ContentCheck` CHECK (CHAR_LENGTH(`content`) > 0)
+     PRIMARY KEY (`Username`)
 ) ENGINE = InnoDB;
 
 CREATE TABLE `FOLLOW` (
-     `PK_User_follows` VARCHAR(50) NOT NULL,
-     `PK_User_followed` VARCHAR(50) NOT NULL,
+     `EkUserFollows` VARCHAR(50) NOT NULL,
+     `EkUserFollowed` VARCHAR(50) NOT NULL,
      `timestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-     PRIMARY KEY (`PK_User_follows`, `PK_User_followed`)
+     PRIMARY KEY (`EkUserFollows`, `EkUserFollowed`)
+) ENGINE = InnoDB;
+
+CREATE TABLE `POST` (
+     `IdPost` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+     `EkUser` VARCHAR(50) NOT NULL,
+     `imageURL` VARCHAR(400) NOT NULL,
+     `caption` VARCHAR(1000) NOT NULL DEFAULT '',
+     `nLikes` INT UNSIGNED NOT NULL DEFAULT 0,
+     `timestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+     PRIMARY KEY (`IdPost`)
 ) ENGINE = InnoDB;
 
 CREATE TABLE `LIKE` (
-     `PK_Post` INT UNSIGNED NOT NULL,
-     `PK_User` VARCHAR(50) NOT NULL,
+     `EkPost` INT UNSIGNED NOT NULL,
+     `EkUser` VARCHAR(50) NOT NULL,
      `timestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-     PRIMARY KEY (`PK_Post`, `PK_User`)
+     PRIMARY KEY (`EkPost`, `EkUser`)
+) ENGINE = InnoDB;
+
+CREATE TABLE `COMMENT` (
+     `IdComment` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+     `EkUser` VARCHAR(50) NOT NULL,
+     `EkPost` INT UNSIGNED NOT NULL,
+     `EkCommentParent` INT UNSIGNED DEFAULT NULL,
+     `content` VARCHAR(1000) NOT NULL,
+     `timestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+     PRIMARY KEY (`IdComment`),
+     CONSTRAINT `ContentCheck` CHECK (CHAR_LENGTH(`content`) > 0)
 ) ENGINE = InnoDB;
 
 CREATE TABLE `NOTIFICATION` (
-     `ID_message` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+     `IdMessage` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+     `EkUserDst` VARCHAR(50) NOT NULL,
+     `EkUserSrc` VARCHAR(50) NOT NULL,
      `content` VARCHAR(500) NOT NULL,
-     `PK_User_dst` VARCHAR(50) NOT NULL,
-     `PK_User_src` VARCHAR(50) NOT NULL,
      `timestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-     PRIMARY KEY (`ID_message`),
+     PRIMARY KEY (`IdMessage`),
      CONSTRAINT `ContentCheck` CHECK (CHAR_LENGTH(`content`) > 0)
 ) ENGINE = InnoDB;
 
@@ -88,59 +88,99 @@ CREATE TABLE `NOTIFICATION` (
 -- Constraints Section
 -- ___________________
 
-ALTER TABLE `COMMENT` ADD CONSTRAINT `FK_Comment_Post`
-     FOREIGN KEY (`PK_Post`)
-     REFERENCES `POST` (`ID_post`)
-     ON DELETE CASCADE;
+ALTER TABLE `FOLLOW`
+     ADD CONSTRAINT `FK_Follow_UserFollowed`
+          FOREIGN KEY (`EkUserFollowed`)
+          REFERENCES `USER` (`Username`)
+          ON UPDATE CASCADE
+          ON DELETE CASCADE,
+     ADD CONSTRAINT `FK_Follow_UserFollows`
+          FOREIGN KEY (`EkUserFollows`)
+          REFERENCES `USER` (`Username`)
+          ON UPDATE CASCADE
+          ON DELETE CASCADE;
 
-ALTER TABLE `COMMENT` ADD CONSTRAINT `FK_Comment_User`
-     FOREIGN KEY (`PK_User`)
-     REFERENCES `USER` (`username`)
-     ON UPDATE CASCADE
-     ON DELETE CASCADE;
+ALTER TABLE `POST`
+     ADD CONSTRAINT `FK_Post_User`
+          FOREIGN KEY (`EkUser`)
+          REFERENCES `USER` (`Username`)
+          ON UPDATE CASCADE
+          ON DELETE CASCADE;
 
-ALTER TABLE `COMMENT` ADD CONSTRAINT `FK_Comment_CommentParent`
-     FOREIGN KEY (`PK_Comment_parent`)
-     REFERENCES `COMMENT` (`ID_comment`)
-     ON DELETE CASCADE;
+ALTER TABLE `COMMENT`
+     ADD CONSTRAINT `FK_Comment_Post`
+          FOREIGN KEY (`EkPost`)
+          REFERENCES `POST` (`IdPost`)
+          ON DELETE CASCADE,
+     ADD CONSTRAINT `FK_Comment_User`
+          FOREIGN KEY (`EkUser`)
+          REFERENCES `USER` (`Username`)
+          ON UPDATE CASCADE
+          ON DELETE CASCADE,
+     ADD CONSTRAINT `FK_Comment_CommentParent`
+          FOREIGN KEY (`EkCommentParent`)
+          REFERENCES `COMMENT` (`IdComment`)
+          ON DELETE CASCADE;
 
-ALTER TABLE `FOLLOW` ADD CONSTRAINT `FK_Follow_UserFollowed`
-     FOREIGN KEY (`PK_User_followed`)
-     REFERENCES `USER` (`username`)
-     ON UPDATE CASCADE
-     ON DELETE CASCADE;
+ALTER TABLE `LIKE`
+     ADD CONSTRAINT `FK_Like_Post`
+          FOREIGN KEY (`EkPost`)
+          REFERENCES `POST` (`IdPost`)
+          ON DELETE CASCADE,
+     ADD CONSTRAINT `FK_Like_User`
+          FOREIGN KEY (`EkUser`)
+          REFERENCES `USER` (`Username`)
+          ON UPDATE CASCADE
+          ON DELETE CASCADE;
 
-ALTER TABLE `FOLLOW` ADD CONSTRAINT `FK_Follow_UserFollows`
-     FOREIGN KEY (`PK_User_follows`)
-     REFERENCES `USER` (`username`)
-     ON UPDATE CASCADE
-     ON DELETE CASCADE;
+ALTER TABLE `NOTIFICATION`
+     ADD CONSTRAINT `FK_Notification_UserDst`
+          FOREIGN KEY (`EkUserDst`)
+          REFERENCES `USER` (`Username`)
+          ON UPDATE CASCADE
+          ON DELETE CASCADE,
+     ADD CONSTRAINT `FK_Notification_UserSrc`
+          FOREIGN KEY (`EkUserSrc`)
+          REFERENCES `USER` (`Username`)
+          ON UPDATE CASCADE
+          ON DELETE CASCADE;
 
-ALTER TABLE `LIKE` ADD CONSTRAINT `FK_Like_Post`
-     FOREIGN KEY (`PK_Post`)
-     REFERENCES `POST` (`ID_post`)
-     ON DELETE CASCADE;
 
-ALTER TABLE `LIKE` ADD CONSTRAINT `FK_Like_User`
-     FOREIGN KEY (`PK_User`)
-     REFERENCES `USER` (`username`)
-     ON UPDATE CASCADE
-     ON DELETE CASCADE;
+-- Triggers Section
+-- ________________
 
-ALTER TABLE `NOTIFICATION` ADD CONSTRAINT `FK_Notification_UserDst`
-     FOREIGN KEY (`PK_User_dst`)
-     REFERENCES `USER` (`username`)
-     ON UPDATE CASCADE
-     ON DELETE CASCADE;
+delimiter //
 
-ALTER TABLE `NOTIFICATION` ADD CONSTRAINT `FK_Notification_UserSrc`
-     FOREIGN KEY (`PK_User_src`)
-     REFERENCES `USER` (`username`)
-     ON UPDATE CASCADE
-     ON DELETE CASCADE;
+CREATE TRIGGER TRG_update_nfollowers_counter AFTER INSERT ON `FOLLOW`
+FOR EACH ROW
+BEGIN
+    UPDATE `USER`
+    SET `nFollowers` = `nFollowers` + 1
+    WHERE `Username` = NEW.`EkUserFollows`;
+END;//
 
-ALTER TABLE `POST` ADD CONSTRAINT `FK_Post_User`
-     FOREIGN KEY (`PK_User`)
-     REFERENCES `USER` (`username`)
-     ON UPDATE CASCADE
-     ON DELETE CASCADE;
+CREATE TRIGGER TRG_update_nfollowed_counter AFTER INSERT ON `FOLLOW`
+FOR EACH ROW
+BEGIN
+    UPDATE `USER`
+    SET `nFollowed` = `nFollowed` + 1
+    WHERE `Username` = NEW.`EkUserFollowed`;
+END;//
+
+CREATE TRIGGER TRG_update_posts_counter AFTER INSERT ON `POST`
+FOR EACH ROW
+BEGIN
+    UPDATE `USER`
+    SET `nPosts` = `nPosts` + 1
+    WHERE `Username` = NEW.`EkUser`;
+END;//
+
+CREATE TRIGGER TRG_update_like_counter AFTER INSERT ON `LIKE`
+FOR EACH ROW
+BEGIN
+    UPDATE `POST`
+    SET `nLikes` = `nLikes` + 1
+    WHERE `EkUser` = NEW.`EkUser`;
+END;//
+
+delimiter ;
