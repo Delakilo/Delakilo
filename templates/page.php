796946d11278be9102e7055a91c5c1ca7f923d49
Alessandro <alessandro.verna@studio.unibo.it>
2024-04-20 16:47:16 +0200

    Create page.php

---
+++ templates/page.php
@@ -0,0 +1,14 @@
+<!-- Alessandro Verna -->
+<header>
+    <h1>Delakilo</h1>
+</header>
+<nav>
+    <ul><li id="home"><a href="home.html"><img src="../img/menu/home.svg" alt="home"/><span class="menu-text">Home</span></a></li><li id="explore"><a href="exploreposts.html"><img src="../img/menu/search.svg" alt="explore"/><span class="menu-text">Explore</span></a></li><li id="notifications"><a href="notifications.html"><img src="../img/menu/bell.svg" alt="notifications"/><span class="menu-text">Notifications</span></a></li><li id="profile"><a href="myprofile.html"><img src="../img/menu/user.svg" alt="profile"/><span class="menu-text">Profile</span></a></li></ul>
+</nav>
+<?php
+    if (isset($templateParams['subpage'])) {
+        require($templateParams['subpage']);
+    } else {
+        die('Unspecified .php page in body');
+    }
+?>
