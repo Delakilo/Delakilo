2e830e62aafc232cc7a6ec09f07d9956c28b22a7
Francesco <francesco.filippini8@studio.unibo.it>
2024-04-25 18:12:08 +0200

    Create homeprofile

---
+++ homeprof.php
@@ -0,0 +1,16 @@
+<?php
+    require_once('config.php');
+
+    if ($db->userIsAlreadyLogged()) {
+        $GLOBALS['log']->logInfo('Welcome in homeprofile page');
+        $templateParams['title'] = 'Home';
+        $templateParams['page'] = 'page.php';
+        $templateParams['subpage'] = 'homeprofile.php';
+        $templateParams['css'][] = 'home.css';
+    } else {
+        header('Location: ./');
+        exit;
+    }
+
+    require('templates/base.php');
+?>
--- templates/homeprofile.php
+++ templates/homeprofile.php
@@ -1,20 +1,32 @@
-<article>
-    <header>
+<!-- Francesco Filippini -->
+<section>
+    <h2>User2</h2>
+    <div>
         <img src="<?php echo IMG_DEFAULT_PROFILE; ?>" alt=""/>
-        <h2><a href="./homeprof.php">User 2</a></h2>
-    </header>
+        <ul>
+            <li><em>XX<br/>Posts</em></li><li><a href=""><em>XX<br/>Followers</em></a></li><li><a href=""><em>XX<br/>Following</em></a></li>
+        </ul>
+    </div>
+    <p><strong>Name Surname</strong></p>
+    <p><small>Bio...</small></p>
+    <footer>
+        <p><a href="">Follow</a></p>
+    </footer>
+</section>
+<article>
+    <!-- TODO: Warning: article lacks heading  -->
     <figure>
-        <img src="<?php echo 'doc/img/posts/landscape.jpg'; ?>" alt=""/>
+        <img src="../img/posts/post3.jpg" alt=""/>
         <figcaption>Check out this photo I posted!</figcaption>
     </figure>
     <footer>
-        <form action="" method="POST">
-            <ul>
+        <form action="#" method="POST">
+            <ul> 
                 <li><label for="like" hidden>Like:</label><input id="like" type="image" src="<?php echo ICON_HEART_EMPTY; ?>" alt="Like"/></li>
             </ul>
         </form>
         <form action="" method="GET">
-            <ul>
+            <ul> 
                 <li><label for="comment" hidden>Comment:</label><input id="comment" type="image" src="<?php echo ICON_COMMENT; ?>" alt="Comment"/></li>
             </ul>
         </form>
