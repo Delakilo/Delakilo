ca406ac390671170c697ad14990ba2a9a9a284c2
Alessandro <alessandro.verna@studio.unibo.it>
2024-05-09 19:24:47 +0200

    Implement edit profile

--- profile.php
+++ profile.php
@@ -7,14 +7,15 @@
     }
 
     $templateParams['title'] = 'Profile';
+    $templateParams['page'] = 'page.php';
+    $templateParams['user_id'] = $_SESSION['user_id'];
     if (isset($_GET['edit'])) {
         $templateParams['title'] .= ' Edit';
-        die('TODO');
+        $templateParams['subpage'] = 'templates/page_profile_edit.php';
+        $templateParams['css'][] = 'myprofileedit.css';
     } else {
-        $templateParams['page'] = 'page.php';
         $templateParams['subpage'] = 'templates/page_profile.php';
         $templateParams['js'][] = 'js/sticky_menu.js';
-        $templateParams['user_id'] = $_SESSION['user_id'];
         $templateParams['css'][] = 'myprofile.css';
     }
 
---
+++ templates/page_profile_edit.php
@@ -0,0 +1,16 @@
+<?php foreach($db->userGetInfoById($templateParams['user_id']) as $info): ?>
+<section>
+    <img src="<?php echo get_user_profile($templateParams['user_id'], $info['imageURL']); ?>" alt=""/>
+    <form action="" method="POST">
+        <h2>Edit profile</h2>
+        <ul>
+            <li><label for="image">Image:</label><input id="image" type="file" name="image"/></li>
+            <li><label for="username">Username:</label><input id="username" type="text" name="username" placeholder="Username" value="<?php echo $info['username']; ?>"/></li>
+            <li><label for="name">Name:</label><input id="name" type="text" name="name" placeholder="Name" value="<?php echo $info['name']; ?>"/></li>
+            <li><label for="surname">Surname:</label><input id="surname" type="text" name="surname" placeholder="Surname" value="<?php echo $info['surname']; ?>"/></li>
+            <li><label for="bio">Bio:</label><textarea id="bio" name="bio" placeholder="Bio" value="<?php echo $info['bio']; ?>"></textarea></li>
+            <li><label for="edit" hidden>Edit:</label><input id="edit" type="submit" value="Edit"/></li>
+        </ul>
+    </form>
+</section>
+<?php endforeach; ?>
