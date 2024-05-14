5a6df9ace099349f38b24390225ae1c0d0b48aaf
Francesco <francesco.filippini8@studio.unibo.it>
2024-05-14 14:36:44 +0200

    Update nLikes to a post

--- js/post.js
+++ js/post.js
@@ -13,6 +13,20 @@ function togglePostLike(idPost) {
             if (response.status == 'OK') {
                 document.getElementById(idPost).src = newImage;
                 document.getElementById(idPost).alt = nextAction;
+                let nLikesList = document.querySelectorAll('body > main > article > aside > p > small');
+                nLikesList.forEach(function(nLikes) {
+                    let matches = nLikes.textContent.match(/\d+/);
+                    if(matches) {
+                        let currentLikes = parseInt(matches[0]);
+                        let updatedString;
+                        if (action === 'LikePost') {
+                            updatedString = nLikes.textContent.replace(/\d+/, currentLikes + 1);
+                        } else {
+                            updatedString = nLikes.textContent.replace(/\d+/, currentLikes - 1);
+                        }
+                        nLikes.textContent = updatedString;
+                    }
+                })
             } else {
                 console.warn(response.status);
             }
